5d096367f9ca46ff0dbb56db6cc5c8738741cffe
leonardvaney <leonard.vaney@epfl.ch>
2021-03-05 14:00:06 +0100

    add some tests for the main activity

--- app/src/androidTest/java/com/github/leonardvaney/bootcamp/MainActivityTest.java
+++ app/src/androidTest/java/com/github/leonardvaney/bootcamp/MainActivityTest.java
@@ -1,6 +1,12 @@
 package com.github.leonardvaney.bootcamp;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -8,6 +14,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static com.github.leonardvaney.bootcamp.MainActivity.EXTRA_MESSAGE;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -17,7 +27,21 @@ public class MainActivityTest {
 
     @Test
     public void testUI(){
-        Espresso.onView(ViewMatchers.withId(R.id.mainName)).perform();
+        Intents.init();
+
+        Intent resultData = new Intent();
+        String nameTest = "Nametest";
+        resultData.putExtra(EXTRA_MESSAGE, nameTest);
+        Instrumentation.ActivityResult result =
+                new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+
+
+        intending(toPackage("com.github.leonardvaney.bootcamp")).respondWith(result);
+
+        Espresso.onView(ViewMatchers.withId(R.id.mainName)).perform(ViewActions.click(), ViewActions.typeText("test"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(ViewMatchers.withId(R.id.mainGoButton)).perform(ViewActions.click());
+
+        Intents.release();
     }
 }
 
