df6fa46987b3bd14ebf3d4267824cf19c3c8c73a
leonardvaney <leonard.vaney@epfl.ch>
2021-03-05 14:39:14 +0100

    some test for the greeting activity and 100% coverage achieved for the bootcamp

--- app/src/androidTest/java/com/github/leonardvaney/bootcamp/GreetingActivityTest.java
+++ app/src/androidTest/java/com/github/leonardvaney/bootcamp/GreetingActivityTest.java
@@ -1,5 +1,9 @@
 package com.github.leonardvaney.bootcamp;
 
+        import android.content.Intent;
+
+        import androidx.test.core.app.ActivityScenario;
+        import androidx.test.core.app.ApplicationProvider;
         import androidx.test.espresso.Espresso;
         import androidx.test.espresso.ViewAction;
         import androidx.test.espresso.matcher.ViewMatchers;
@@ -10,11 +14,23 @@ package com.github.leonardvaney.bootcamp;
         import org.junit.Test;
         import org.junit.runner.RunWith;
 
+        import static androidx.test.espresso.assertion.ViewAssertions.matches;
+        import static androidx.test.espresso.matcher.ViewMatchers.withText;
+        import static com.github.leonardvaney.bootcamp.MainActivity.EXTRA_MESSAGE;
+
 @RunWith(AndroidJUnit4.class)
 public class GreetingActivityTest {
 
     @Test
     public void testGreeting(){
 
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), GreetingActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, "test"); //au lieu de faire ça on pourrait peut-être faire qqch de plus intelligent?
+
+        ActivityScenario scenario = ActivityScenario.launch(intent);
+
+        Espresso.onView(ViewMatchers.withId(R.id.textView3)).check(matches(withText("test")));
+
+        scenario.close();
     }
 }
