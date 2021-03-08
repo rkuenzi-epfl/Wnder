a0e84784a5bf31fe8eef5007f31be79fca417f66
Jeremy Dezalos <jeremy.dezalos@epfl.ch>
2021-03-08 16:55:02 +0100

    Test pour mon ami Nico

--- app/src/main/java/com/github/wnder/MainActivity.java
+++ app/src/main/java/com/github/wnder/MainActivity.java
@@ -1,14 +1,62 @@
 package com.github.wnder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.db = FirebaseFirestore.getInstance();
+        uploadToFirestore();
+        downloadFromFirestore();
+    }
+
+    private void uploadToFirestore(){
+        Map<String, String> testMap = new HashMap<>();
+        testMap.put("Jeremy", "Jus d'orange");
+        testMap.put("Léonard", "Lasagne");
+        testMap.put("Romain", "Pizza du jeudi soir");
+        testMap.put("Nico", "Cookies");
+
+
+        db.collection("test").document("doc").set(testMap);
+    }
+
+    private void downloadFromFirestore(){
+        db.collection("test")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                System.out.println(document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w("Error", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
