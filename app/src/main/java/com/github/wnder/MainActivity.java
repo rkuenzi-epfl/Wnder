3e7c7b2981886cab62fd6f826fe340a788e7d0cf
Jeremy Dezalos <jeremy.dezalos@epfl.ch>
2021-03-09 18:19:58 +0100

    Adding/retrieving data from FireCloud/CloudStorage implemented not tested though because waiting on Alois' work

--- app/src/main/java/com/github/wnder/MainActivity.java
+++ app/src/main/java/com/github/wnder/MainActivity.java
@@ -3,6 +3,7 @@ package com.github.wnder;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -14,13 +15,24 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseFirestore db;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +40,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.db = FirebaseFirestore.getInstance();
+        this.storage = FirebaseStorage.getInstance();
+        this.storageRef = storage.getReference();
+
         uploadToFirestore();
         downloadFromFirestore();
+    }
+
+    private void uploadToCloudStorage(String filepath){
+        Uri file = Uri.fromFile(new File(filepath));
+
+        StorageMetadata metadata = new StorageMetadata.Builder()
+                .setContentType("image/jpeg")
+                .build();
+
+        UploadTask uploadTask = storageRef.child(filepath).putFile(file, metadata);
+
+        uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                double progress = (100.0 * snapshot.getBytesTransferred()) / snapshot.getTotalByteCount();
+                System.out.println("Upload is " + progress + "% done");
+            }
+        }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onPaused(@NonNull UploadTask.TaskSnapshot snapshot) {
+                System.out.println("Upload is paused");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println("Upload failed");
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                System.out.println("Upload Succeeded");
+            }
+        });
+    }
+
+    private void downloadFromCloudStorage(String filepath) throws IOException {
+        StorageReference ref = storageRef.child(filepath);
+
+        File localFile =  File.createTempFile("images", "jpg");
+
+        ref.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                System.out.println("Download succeeded");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println("Download Failed");
+            }
+        });
     }
 
     private void uploadToFirestore(){
@@ -38,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
         testMap.put("Léonard", "Lasagne");
         testMap.put("Romain", "Pizza du jeudi soir");
         testMap.put("Nico", "Cookies");
+        testMap.put("Alois", "merci MV");
 
 
         db.collection("test").document("doc").set(testMap);
