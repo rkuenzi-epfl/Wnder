50ef4a1493e56ed83eee0b1de8ab40a73907e333
Pablo Stebler <pablo@stebler.xyz>
2021-03-11 23:45:35 +0100

    Add main activity content
    Closes #8, closes #22

--- app/src/main/java/com/github/wnder/MainActivity.java
+++ app/src/main/java/com/github/wnder/MainActivity.java
@@ -1,8 +1,11 @@
 package com.github.wnder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        TextView textView = toolbar.findViewById(R.id.username);
+        User user = GlobalUser.getUser();
+        textView.setText(user.getName());
+
+        ImageView imageView = toolbar.findViewById(R.id.profile_picture);
+        imageView.setImageURI(user.getProfilePicture());
     }
 }
