160440ef3085c98edda5458cf4b9385174b70634
rkuenzi <romain.kuenzi@epfl.ch>
2021-03-12 12:13:23 +0100

    Added test for global user

--- app/src/test/java/com/github/wnder/UserTest.java
+++ app/src/test/java/com/github/wnder/UserTest.java
@@ -7,12 +7,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
 
 @RunWith(RobolectricTestRunner.class)
 public class UserTest {
-
 
     @Test
     public void guestUserReturnGuestName(){
@@ -37,4 +38,33 @@ public class UserTest {
         User u = new SignedInUser("TestUser", Uri.parse("android.resource://raw/ladiag.jpg"));
         assertEquals(u.getProfilePicture(), Uri.parse("android.resource://raw/ladiag.jpg"));
     }
+
+    @Test
+    public void globalUserReturnGuestUserByDefault(){
+
+        User u = GlobalUser.getUser();
+        assertThat(u.getName(), is("Guest"));
+        assertThat(u.getProfilePicture(), is(Uri.parse("android.resource://raw/ladiag.jpg")));
+        GlobalUser.resetUser();
+    }
+
+    @Test
+    public void globalUserReturnCorrectUserAfterSet(){
+        GlobalUser.setUser(new SignedInUser("SignedInUser", Uri.parse("android.resource://raw/ladiag.jpg")));
+        User u = GlobalUser.getUser();
+        assertThat(u.getName(), is("SignedInUser"));
+        assertThat(u.getProfilePicture(), is(Uri.parse("android.resource://raw/ladiag.jpg")));
+        GlobalUser.resetUser();
+    }
+
+    @Test
+    public void globalUserReturnCorrectUserAfterReset(){
+        GlobalUser.setUser(new SignedInUser("SignedInUser", Uri.parse("android.resource://raw/ladiag.jpg")));
+        GlobalUser.resetUser();
+        User u = GlobalUser.getUser();
+        assertThat(u.getName(), is("Guest"));
+        assertThat(u.getProfilePicture(), is(Uri.parse("android.resource://raw/ladiag.jpg")));
+        GlobalUser.resetUser();
+    }
+
 }
