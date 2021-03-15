128eb1ba6fb96a29053f3889a09938e6d9d7147e
rkuenzi <romain.kuenzi@epfl.ch>
2021-03-15 19:06:38 +0100

    GuessPreview UI, missing actual content

---
+++ app/src/main/java/com/github/wnder/GuessPreviewActivity.java
@@ -0,0 +1,48 @@
+package com.github.wnder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class GuessPreviewActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_preview);
+        findViewById(R.id.guessButton).setOnClickListener(this);
+        findViewById(R.id.skipButton).setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // TODO: Get random image from DB and display it
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.guessButton:
+                openGuessActivity();
+                break;
+            case R.id.skipButton:
+                openPreviewActivity();
+                break;
+                // Other buttons can be setup in this switch
+        }
+    }
+
+    private void openGuessActivity() {
+        // TODO: open the guessing activity (the map to guess where the picture was taken)
+    }
+
+    private void openPreviewActivity() {
+        Intent intent = new Intent(this, GuessPreviewActivity.class);
+        startActivity(intent);
+    }
+}
+
