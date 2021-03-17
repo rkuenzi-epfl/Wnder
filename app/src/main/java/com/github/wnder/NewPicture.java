5b8399a2ada0f43f45121a1673cd23cd103e5e28
nvannay <nicolas.vannay@epfl.ch>
2021-03-17 11:23:13 +0100

    added tests for NewPicture

--- app/src/main/java/com/github/wnder/NewPicture.java
+++ app/src/main/java/com/github/wnder/NewPicture.java
@@ -2,9 +2,13 @@ package com.github.wnder;
 
 import android.net.Uri;
 
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Map;
 
 public class NewPicture implements Picture{
+    private String user;
+
     //Image unique ID
     private String uniqueId;
     private Uri uri;
@@ -18,11 +22,23 @@ public class NewPicture implements Picture{
     //Map<[user ID], Map<[longitude/latitude], [value]>>
     private Map<String, Map<String, Double>> guesses;
 
-    public NewPicture(double longitude, double latitude, Uri uri){
+    public NewPicture(String user, double longitude, double latitude, Uri uri){
+        this.user = user;
+
         this.longitude = longitude;
         this.latitude = latitude;
         this.uri = uri;
 
+        this.scoreboard = new HashMap<String, Double>();
+        this.scoreboard.put("default", -1.);
+
+        this.guesses = new HashMap<String, Map<String, Double>>();
+        Map<String, Double> defaultGuess = new HashMap<>();
+        defaultGuess.put("longitude", -1.);
+        defaultGuess.put("latitude", -1.);
+        this.guesses.put("default", defaultGuess);
+
+        this.uniqueId = this.user + Calendar.getInstance().getTimeInMillis();
     }
 
     public Boolean sendPictureToDb(){
