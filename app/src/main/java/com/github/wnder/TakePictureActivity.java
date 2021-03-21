885b2b68173431fbdfc2b20dc6fcf99b3afb566a
Jeremy Dezalos <jeremy.dezalos@epfl.ch>
2021-03-21 13:31:57 +0100

    Activity where a picture is taken and then stored added

---
+++ app/src/main/java/com/github/wnder/TakePictureActivity.java
@@ -0,0 +1,133 @@
+package com.github.wnder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.gson.internal.$Gson$Preconditions;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TakePictureActivity extends AppCompatActivity {
+    private Button take_picture_button;
+    private Button confirm_button;
+    private ImageView picture;
+    private final int TAKE_PHOTO = 0;
+    private String currentPhotoPath;
+    private String imageFileName;
+    private Bitmap currentBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_picture);
+        picture = findViewById(R.id.image_from_camera);
+
+        take_picture_button = findViewById(R.id.take_picture_button);
+        take_picture_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        confirm_button = findViewById(R.id.picture_confirm_button);
+        confirm_button.setVisibility(View.GONE);
+        confirm_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                storeBitmapInGallery();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent cameraIntent){
+        super.onActivityResult(requestCode, resultCode, cameraIntent);
+        if(resultCode == RESULT_OK && requestCode == TAKE_PHOTO){
+            setPic();
+            confirm_button.setVisibility(View.VISIBLE);
+         }
+    }
+
+    private void storeBitmapInGallery(){
+        MediaStore.Images.Media.insertImage(getContentResolver(), currentBitmap, imageFileName , "");
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.github.wnder.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, TAKE_PHOTO);
+            }
+
+    }
+
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = picture.getWidth();
+        int targetH = picture.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.max(1, Math.min(photoW/targetW, photoH/targetH));
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        currentBitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        picture.setImageBitmap(currentBitmap);
+    }
+}
+
+
