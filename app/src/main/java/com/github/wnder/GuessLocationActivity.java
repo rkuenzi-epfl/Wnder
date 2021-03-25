f5a9480aaa003558fed390adbf7370282eb66262
Alois <alois.mattei@hotmail.fr>
2021-03-25 21:52:16 +0100

    GuessLocationActivity basic implementation

---
+++ app/src/main/java/com/github/wnder/GuessLocationActivity.java
@@ -0,0 +1,14 @@
+package com.github.wnder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuessLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_location);
+    }
+}
