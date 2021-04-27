39a0fbf45996a15ba13732e1f64adf1039183fb9
nvannay <nicolas.vannay@epfl.ch>
2021-04-27 10:13:06 +0200

    made asked changes

--- app/src/androidTest/java/com/github/wnder/GuessPreviewActivityTest.java
+++ app/src/androidTest/java/com/github/wnder/GuessPreviewActivityTest.java
@@ -58,24 +58,6 @@ public class GuessPreviewActivityTest {
         Intents.release();
     }
 
-    //Process crashed because of Intents.release if we don't do it after.
-    /*@Test
-    public void testSkipButtonKarma() throws InterruptedException {
-        Intents.init();
-        //TODO: replace once PR 90 is merged
-        ExistingPicture pic = new ExistingPicture("testPicDontRm");
-
-        pic.onKarmaAvailable((k1) -> {
-            onView(withId(R.id.skipButton)).perform(click());
-            Intents.intended(hasComponent(GuessPreviewActivity.class.getName()));
-            pic.onKarmaAvailable((k2) -> {
-                assertThat(k2, is(k1-1));
-            });
-
-
-        });
-    }*/
-
     @Test
     public void testSkipButton(){
         Intents.init();
