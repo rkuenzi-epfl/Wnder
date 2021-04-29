497610ffee2134b0052562577d03ff1fe900da3e
nvannay <nicolas.vannay@epfl.ch>
2021-04-29 17:35:07 +0200

    store and read pics

--- app/src/main/java/com/github/wnder/picture/LocalPictureDatabase.java
+++ app/src/main/java/com/github/wnder/picture/LocalPictureDatabase.java
@@ -1,9 +1,13 @@
 package com.github.wnder.picture;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -61,4 +65,25 @@ public class LocalPictureDatabase {
         }
     }
 
+    private Bitmap openPictureFile(String uniqueId) throws FileNotFoundException {
+        File file = new File(uniqueId);
+        FileInputStream fis = context.openFileInput(uniqueId);
+        byte[] bytes = new byte[(int) file.length()];
+        try {
+            fis.read(bytes, 0, bytes.length);
+            fis.close();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+
+    private void storePictureFile(Bitmap bmp, String uniqueId) throws IOException {
+        FileOutputStream fileobj = context.openFileOutput(uniqueId, Context.MODE_PRIVATE);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        fileobj.write(stream.toByteArray()); //writing to file
+        fileobj.close(); //File closed
+    }
 }
