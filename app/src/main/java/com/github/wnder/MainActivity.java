d45bb7684fdc26e70815e4cf4a1eac76d102e02a
Jeremy Dezalos <jeremy.dezalos@epfl.ch>
2021-04-30 15:10:48 +0200

    fix deletion problem

--- app/src/main/java/com/github/wnder/MainActivity.java
+++ app/src/main/java/com/github/wnder/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
 import com.github.wnder.user.GlobalUser;
 import com.github.wnder.user.User;
@@ -73,6 +74,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //rights for location services
         String[] ss = {Manifest.permission.ACCESS_FINE_LOCATION};
+        ActivityCompat.requestPermissions(this, ss, 100); //Very important to have permission for future call
     }
 
     /**
