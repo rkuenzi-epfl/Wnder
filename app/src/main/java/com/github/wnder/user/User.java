a2082469effc9f0c527ebb21bdcb072f2ef6185e
leonardvaney <leonard.vaney@epfl.ch>
2021-05-03 14:56:49 +0200

    an if to handle unexpected result from the GPS

--- app/src/main/java/com/github/wnder/user/User.java
+++ app/src/main/java/com/github/wnder/user/User.java
@@ -47,13 +47,17 @@ public abstract class User {
      * @return last known location of user
      */
     public Location getPositionFromGPS(LocationManager manager, Context context){
-
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
         //TODO:
         //throw new IllegalStateException();
         }
-
-        return manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location loc = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(loc == null){ //To avoid unexpected result from the GPS, we set it to 0, 0.
+            loc = new Location(LocationManager.GPS_PROVIDER);
+            loc.setLongitude(0);
+            loc.setLatitude(0);
+        }
+        return loc;
     }
 
     /**
