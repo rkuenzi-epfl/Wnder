b226c3ed538ee6723b68aee00549a280528423de
rkuenzi <romain.kuenzi@epfl.ch>
2021-05-06 11:04:49 +0200

    Fixed most GuessLocationActivity tests

--- app/src/androidTest/java/com/github/wnder/GuessLocationActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/github/wnder/GuessLocationActivityInstrumentedTest.java
@@ -7,11 +7,26 @@ import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.github.wnder.picture.PicturesDatabase;
+import com.github.wnder.picture.PicturesModule;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+import dagger.hilt.android.testing.BindValue;
+import dagger.hilt.android.testing.HiltAndroidRule;
+import dagger.hilt.android.testing.HiltAndroidTest;
+import dagger.hilt.android.testing.UninstallModules;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -20,8 +35,11 @@ import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@RunWith(AndroidJUnit4.class)
+@HiltAndroidTest
+@UninstallModules({PicturesModule.class})
 public class GuessLocationActivityInstrumentedTest  {
     //Intent with extras that the activity will start with
     static Intent intent;
@@ -33,9 +51,16 @@ public class GuessLocationActivityInstrumentedTest  {
         intent.putExtra(GuessLocationActivity.EXTRA_PICTURE_LNG, 10.0);
         intent.putExtra(GuessLocationActivity.EXTRA_PICTURE_ID, "");
     }
+    private static Map<String, Double> dummyMap;
 
-    @Rule //launches a given activity before the test starts and closes after the test
-    public ActivityScenarioRule<ImageFromGalleryActivity> activityRule = new ActivityScenarioRule(intent);
+    private HiltAndroidRule hiltRule = new HiltAndroidRule(this);
+
+    @Rule
+    public RuleChain testRule = RuleChain.outerRule(hiltRule)
+            .around(new ActivityScenarioRule<>(intent));
+
+    @BindValue
+    public static PicturesDatabase picturesDatabase = Mockito.mock(PicturesDatabase.class);
 
     //For activities that we did ourself otherwise need to use mockito
     @Before //Initializes Intents and begins recording intents, similar to MockitoAnnotations.initMocks.
@@ -46,6 +71,20 @@ public class GuessLocationActivityInstrumentedTest  {
     @After //Clears Intents state. Must be called after each test case.
     public void tearDown() {
         Intents.release();
+    }
+
+    @BeforeClass
+    public static void beforeAll(){
+        dummyMap = new HashMap<>();
+        dummyMap.put("User0", 32.);
+        dummyMap.put("User1", 44.);
+        when(picturesDatabase.getScoreboard(any())).thenReturn(CompletableFuture.completedFuture(dummyMap));
+        intent = new Intent(ApplicationProvider.getApplicationContext(), GuessLocationActivity.class);
+        intent.putExtra(GuessLocationActivity.EXTRA_CAMERA_LAT, 10.0);
+        intent.putExtra(GuessLocationActivity.EXTRA_CAMERA_LNG, 10.0);
+        intent.putExtra(GuessLocationActivity.EXTRA_PICTURE_LAT, 10.0);
+        intent.putExtra(GuessLocationActivity.EXTRA_PICTURE_LNG, 10.0);
+        intent.putExtra(GuessLocationActivity.EXTRA_PICTURE_ID, "");
     }
 
     @Test
