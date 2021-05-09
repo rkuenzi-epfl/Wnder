6a38a99aaa9ba842e517fa102a3e94e0b5f7aeff
rkuenzi <romain.kuenzi@epfl.ch>
2021-05-10 00:44:00 +0200

    Set takePicture on NavBar click

--- app/src/main/java/com/github/wnder/NavigationActivity.java
+++ app/src/main/java/com/github/wnder/NavigationActivity.java
@@ -1,9 +1,16 @@
 package com.github.wnder;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
 
 import com.github.wnder.user.GlobalUser;
 import com.github.wnder.user.SignedInUser;
@@ -14,9 +21,12 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.util.HashMap;
 import java.util.Map;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
 /**
  * Class displaying a bottom navigation bar and letting us go from one fragment to the other depending on this bar
  */
+@AndroidEntryPoint
 public class NavigationActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
@@ -46,6 +56,9 @@ public class NavigationActivity extends AppCompatActivity {
         //call updateFragment depending on clicked icon
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> updateFragment(iconMap.get(item.getItemId())));
+
+        String[] ss = {Manifest.permission.ACCESS_FINE_LOCATION};
+        ActivityCompat.requestPermissions(this, ss, 100); //Very important to have permission for future call
     }
 
     /**
@@ -62,7 +75,43 @@ public class NavigationActivity extends AppCompatActivity {
      * @return Boolean true
      */
     private Boolean updateFragment(String id){
-        //do nothing for now
+        if(id.equals(TAKE_PICTURE_PAGE)){
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container_view, TakePictureFragment.class, null)
+                    .setReorderingAllowed(true)
+                    .addToBackStack(null)
+                    .commit();
+        }
+
         return true;
+    }
+
+    /**
+     * To run when we get user's answer for location permission
+     * @param requestCode request code
+     * @param permissions permissions
+     * @param grantResults grant results
+     */
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //if it's not the good request, return
+        if(requestCode == 100){
+            //permission to get the location
+            for(int i = 0; i < permissions.length; ++i){
+                if(permissions[i].equals(Manifest.permission.ACCESS_FINE_LOCATION) && !(grantResults[i] == PackageManager.PERMISSION_GRANTED)){
+                    // TODO: What happens if the user did not accept?
+                    throw new UnsupportedOperationException();
+                }
+
+                LocationManager LocMan = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+                LocMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 1, location -> {
+                    //Nothing to do in case of location change, the request is being done when necessary with getLastKnownLocation
+                });
+            }
+        }
     }
 }
--- app/src/main/java/com/github/wnder/TakePictureFragment.java
+++ app/src/main/java/com/github/wnder/TakePictureFragment.java
@@ -1,14 +1,12 @@
 package com.github.wnder;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
+import android.content.Context;
 import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -18,16 +16,15 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+import androidx.transition.TransitionManager;
 
 import com.github.wnder.picture.PicturesDatabase;
 import com.github.wnder.user.GlobalUser;
-import com.github.wnder.user.User;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.concurrent.CompletableFuture;
 
 import javax.inject.Inject;
@@ -49,8 +46,8 @@ public class TakePictureFragment extends Fragment {
     private static final int CONSTRAINT_DISTANCE_BOTTOM = 32;
 
     private ConstraintLayout constraintLayout;
-    private Button takePictureButton;
-    private Button uploadButton;
+    private FloatingActionButton takePictureButton;
+    private FloatingActionButton uploadButton;
     private String userName;
 
     private Uri takenPictureUri;
@@ -61,13 +58,13 @@ public class TakePictureFragment extends Fragment {
         super(R.layout.fragment_take_picture);
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         constraintLayout = (ConstraintLayout) getView();
         takePictureButton = view.findViewById(R.id.takePictureButton);
         uploadButton = view.findViewById(R.id.uploadButton);
         userName = GlobalUser.getUser().getName();
-
         takePictureLauncher = registerForActivityResult(new TakePicture(), (stored) -> onTakePictureResult(stored));
 
         takePictureButton.setOnClickListener(button -> openCamera());
@@ -104,18 +101,14 @@ public class TakePictureFragment extends Fragment {
      */
     private void onTakePictureResult(boolean stored) {
         if (stored) {
+            // Move takePictureButton to the left
+            TransitionManager.beginDelayedTransition(constraintLayout);
+            ConstraintSet constraintSet = new ConstraintSet();
+            constraintSet.load(getContext(), R.layout.fragment_take_picture_upload);
+            constraintSet.applyTo(constraintLayout);
+
             ImageView takenPictureView = getView().findViewById(R.id.takenPicture);
             takenPictureView.setImageURI(takenPictureUri);
-
-            // Move takePictureButton to the left
-            ConstraintSet constraintSet = new ConstraintSet();
-            constraintSet.clone(constraintLayout);
-            constraintSet.clear(R.id.takePictureButton);
-            constraintSet.connect(R.id.takePictureButton, ConstraintSet.END, R.id.uploadButton, ConstraintSet.START, CONSTRAINT_DISTANCE_RIGHT);
-            constraintSet.connect(R.id.takePictureButton, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, CONSTRAINT_DISTANCE_BOTTOM);
-            constraintSet.applyTo(constraintLayout);
-            // Show upload button
-            uploadButton.setVisibility(View.VISIBLE);
             uploadButton.setOnClickListener(button -> uploadTakenPicture());
         }
     }
@@ -124,19 +117,17 @@ public class TakePictureFragment extends Fragment {
      * Upload the picture taken
      */
     private void uploadTakenPicture() {
-
+        takenPictureLocation = GlobalUser.getUser().getPositionFromGPS((LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE), getContext());
         CompletableFuture<Void> uploadResult = picturesDb.uploadPicture(takenPictureId, userName, takenPictureLocation, takenPictureUri);
         uploadResult.thenAccept(res -> {
-            // Hide uploadButton
-            uploadButton.setVisibility(View.INVISIBLE);
-            uploadButton.setClickable(false);
+
             // Move takePictureButton to the right
+            TransitionManager.beginDelayedTransition(constraintLayout);
             ConstraintSet constraintSet = new ConstraintSet();
-            constraintSet.clone(constraintLayout);
-            constraintSet.clear(R.id.takePictureButton);
-            constraintSet.connect(R.id.takePictureButton, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, CONSTRAINT_DISTANCE_RIGHT);
-            constraintSet.connect(R.id.takePictureButton, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, CONSTRAINT_DISTANCE_BOTTOM);
+            constraintSet.load(getContext(), R.layout.fragment_take_picture);
             constraintSet.applyTo(constraintLayout);
+
+            uploadButton.setClickable(false);
         }).exceptionally(res -> {
             // TODO: Display upload failed message
             return null;
