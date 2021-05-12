a15a54586eaf6500c8d39dd3307cc5229196549b
leonardvaney <leonard.vaney@epfl.ch>
2021-05-12 16:33:22 +0200

    some simplification of the function onFling

--- app/src/main/java/com/github/wnder/OnSwipeTouchListener.java
+++ app/src/main/java/com/github/wnder/OnSwipeTouchListener.java
@@ -27,6 +27,29 @@ public class OnSwipeTouchListener implements View.OnTouchListener {
         private static final int SWIPE_THRESHOLD = 100;
         private static final int SWIPE_VELOCITY_THRESHOLD = 100;
 
+        private boolean horizontalSwipe(float diffX, float velocityX){
+            boolean result = false;
+            if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                if (diffX > 0) {
+                    result = onSwipeRight();
+                } else {
+                    result = onSwipeLeft();
+                }
+            }
+            return result;
+        }
+
+        private boolean verticalSwipe(float diffY, float velocityY){
+            boolean result = false;
+            if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                if (diffY > 0) {
+                    result = onSwipeBottom();
+                } else {
+                    result =  onSwipeTop();
+                }
+            }
+            return result;
+        }
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
@@ -35,21 +58,9 @@ public class OnSwipeTouchListener implements View.OnTouchListener {
                 float diffY = e2.getY() - e1.getY();
                 float diffX = e2.getX() - e1.getX();
                 if (Math.abs(diffX) > Math.abs(diffY)) {
-                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                        if (diffX > 0) {
-                            result = onSwipeRight();
-                        } else {
-                            result = onSwipeLeft();
-                        }
-                    }
+                    result = horizontalSwipe(diffX, velocityX);
                 } else {
-                    if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                        if (diffY > 0) {
-                            result = onSwipeBottom();
-                        } else {
-                            result = onSwipeTop();
-                        }
-                    }
+                    result = verticalSwipe(diffY, velocityY);
                 }
             } catch (Exception exception) {
                 exception.printStackTrace();
