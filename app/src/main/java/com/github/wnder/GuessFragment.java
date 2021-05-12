c3fd6e9cd867ccc62774486d31311eb4d0d514e5
rkuenzi <romain.kuenzi@epfl.ch>
2021-05-13 00:57:04 +0200

    Made empty fragments to have all navigation accessible

---
+++ app/src/main/java/com/github/wnder/GuessFragment.java
@@ -0,0 +1,10 @@
+package com.github.wnder;
+
+import androidx.fragment.app.Fragment;
+
+public class GuessFragment extends Fragment {
+
+    public GuessFragment(){
+
+    }
+}
--- app/src/main/java/com/github/wnder/NavigationActivity.java
+++ app/src/main/java/com/github/wnder/NavigationActivity.java
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,7 @@ import androidx.fragment.app.FragmentManager;
 import com.github.wnder.networkService.NetworkService;
 import com.github.wnder.user.GlobalUser;
 import com.github.wnder.user.GuestUser;
+import com.github.wnder.user.SignedInUser;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.HashMap;
@@ -34,12 +36,6 @@ public class NavigationActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
 
-    //Page strings
-    private static final String PROFILE_PAGE = "profile";
-    private static final String TAKE_PICTURE_PAGE = "take_picture";
-    private static final String GUESS_PAGE = "guess";
-    private static final String HISTORY_PAGE = "history";
-
     private static final int REQUEST_POSITION_CODE = 100;
 
     /**
@@ -51,12 +47,15 @@ public class NavigationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation);
 
+        // If you want to test history without sign in:
+        // GlobalUser.setUser(new SignedInUser("Bonbon", Uri.parse("android.resource://com.github.wnder/" + R.raw.ladiag)));
+
         //Setup map with corresponding strings
-        Map<Integer, String> iconMap = new HashMap<>();
-        iconMap.put(R.id.profile_page, PROFILE_PAGE);
-        iconMap.put(R.id.take_picture_page, TAKE_PICTURE_PAGE);
-        iconMap.put(R.id.guess_page, GUESS_PAGE);
-        iconMap.put(R.id.history_page, HISTORY_PAGE);
+        Map<Integer, Class> iconMap = new HashMap<>();
+        iconMap.put(R.id.profile_page, ProfileFragment.class);
+        iconMap.put(R.id.take_picture_page, TakePictureFragment.class);
+        iconMap.put(R.id.guess_page, GuessFragment.class);
+        iconMap.put(R.id.history_page, HistoryFragment.class);
 
         //call updateFragment depending on clicked icon
         bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -76,27 +75,18 @@ public class NavigationActivity extends AppCompatActivity {
 
     /**
      * Update fragment depending on pressed navigation bar button
-     * @param id string defining pressed button
+     * @param fragmentClass class associated to pressed button
      * @return Boolean true
      */
-    private Boolean updateFragment(String id){
-        if(id.equals(TAKE_PICTURE_PAGE)){
-            // Alert Guest user and user no connected to the internet
-            if(GlobalUser.getUser() instanceof GuestUser){
-                AlertBuilder.okAlert(getString(R.string.guest_not_allowed), getString(R.string.guest_no_upload), this)
-                        .show();
-            } else if(!networkInfo.isNetworkAvailable()){
-                AlertBuilder.okAlert(getString(R.string.no_connection), getString(R.string.no_internet_upload), this)
-                        .show();
-            }
+    private Boolean updateFragment(Class fragmentClass){
 
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container_view, TakePictureFragment.class, null)
-                    .setReorderingAllowed(true)
-                    .addToBackStack(null)
-                    .commit();
-        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, fragmentClass, null)
+                .setReorderingAllowed(true)
+                .addToBackStack(null)
+                .commit();
 
         return true;
     }
