1a1ad5afcb4dd16c443009127b7367441cdb2aea
leonardvaney <leonard.vaney@epfl.ch>
2021-05-20 20:49:53 +0200

    remove import

--- app/src/main/java/com/github/wnder/ProfileFragment.java
+++ app/src/main/java/com/github/wnder/ProfileFragment.java
@@ -2,7 +2,6 @@ package com.github.wnder;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
