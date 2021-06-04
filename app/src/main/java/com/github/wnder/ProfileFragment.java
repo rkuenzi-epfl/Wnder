0c948f117a093616ab9879e62830bd05e4e3d3c7
rkuenzi <romain.kuenzi@epfl.ch>
2021-06-04 14:48:47 +0200

    Removed useless log

--- app/src/main/java/com/github/wnder/ProfileFragment.java
+++ app/src/main/java/com/github/wnder/ProfileFragment.java
@@ -2,7 +2,6 @@ package com.github.wnder;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,8 +21,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.card.MaterialCardView;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
@@ -192,7 +189,6 @@ public class ProfileFragment extends Fragment {
      * and update UI
      */
     private void setGlobalUser(FirebaseUser firebaseUser){
-        Log.d("FACC", "GOT FACC");
         GlobalUser.setUser(new SignedInUser(firebaseUser.getDisplayName(), firebaseUser.getPhotoUrl(), firebaseUser.getUid()));
         updateLoginStatus();
     }
@@ -202,12 +198,10 @@ public class ProfileFragment extends Fragment {
      * @param credential credential from the auth provider
      */
     private void handleSignInResult(AuthCredential credential) {
-        Log.d("GACC", "GOT s");
 
         firebaseAuth.signInWithCredential(credential)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        Log.d("succ fo success", "signInWithCredential:success");
                         FirebaseUser user = firebaseAuth.getCurrentUser();
                         setGlobalUser(user);
                     } else {signInFailed();}
